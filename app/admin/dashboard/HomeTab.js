eb913858d3d8fcea8d8f054f067ef71e89577046
Vikram Pal <vikrampal659@gmail.com>
2018-02-06 21:41:50 +0530

    Add redirect to login if unauthorized on admin home page (#22)

--- app/admin/dashboard/HomeTab.js
+++ app/admin/dashboard/HomeTab.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { Container } from 'semantic-ui-react';
 import axios from 'axios';
 import SubmitPost from '../post/SubmitPost';
@@ -7,23 +8,11 @@ import PostList from '../post/PostList';
 // import PostView from '../../user/post/PostView';
 import Auth from '../../auth/modules/Auth';
 
-const fetchPost = () =>
-  (axios.get('/api/admin/posts', {
-    headers: {
-      Authorization: `bearer ${Auth.getToken()}`,
-    },
-  })
-    .then((resp) => {
-      console.log('response is ', resp);
-      return resp.data;
-    })
-    .catch(console.error));
-
 class HomeTab extends Component {
   constructor() {
     super();
     this.state = { posts: [] };
-    this.fetchPost = fetchPost.bind(this);
+    this.fetchPost = this.fetchPost.bind(this);
     this.refetch = () => {
       // console.log(this);
       // console.log('in refetch');
@@ -40,6 +29,26 @@ class HomeTab extends Component {
         console.log('Sagnik', this, this.state);
       });
   }
+
+  fetchPost() {
+    return axios.get('/api/admin/posts', {
+      headers: {
+        Authorization: `bearer ${Auth.getToken()}`,
+      },
+    })
+      .then((resp) => {
+        console.log('response is ', resp);
+        return resp.data;
+      })
+      .catch((error) => {
+        console.error(error);
+        // redirect to login page if 401 (unauthorized) response
+        if (error.response.status === 401) {
+          this.props.history.replace('/login');
+        }
+      });
+  }
+
   render() {
     return (
       <div className="Home" >
@@ -55,5 +64,10 @@ class HomeTab extends Component {
   }
 }
 
+HomeTab.propTypes = {
+  history: PropTypes.shape({
+    replace: PropTypes.func.isRequired,
+  }).isRequired,
+};
 
 export default HomeTab;
