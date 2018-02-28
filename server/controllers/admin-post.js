387f8f1f5c4c8b8af472dd282703c8fed2c63c94
Vikram Pal <vikrampal659@gmail.com>
2018-02-28 07:19:51 +0530

    Fix apply to post and favourite post bug

--- server/controllers/admin-post.js
+++ server/controllers/admin-post.js
@@ -3,34 +3,34 @@ const PostModel = require('mongoose').model('Post');
 exports.createPost = (req, res) => {
   const newPost = new PostModel(req.body);
   newPost.save((err) => {
-    if (err) res.status(200).json({ message: 'database error' });
+    if (err) res.status(500).json({ message: 'database error' });
     else res.status(200).json({ message: 'post created' });
   });
 };
 
 exports.getAllPost = (req, res) => {
   PostModel.find({ }, (err, docs) => {
-    if (err) res.status(200).json({ message: 'database error' });
+    if (err) res.status(500).json({ message: 'database error' });
     else res.status(200).json(docs);
   });
 };
 exports.getPost = (req, res) => {
   PostModel.findById({ _id: req.params.id }, (err, docs) => {
-    if (err || !docs) res.status(200).json({ message: 'database error' });
+    if (err || !docs) res.status(500).json({ message: 'database error' });
     else { res.status(200).json(docs); }
   });
 };
 
 exports.updatePost = (req, res) => {
   PostModel.findByIdAndUpdate({ _id: req.params.id }, req.body, (err, docs) => {
-    if (err || !docs) res.status(200).json({ message: 'database error' });
+    if (err || !docs) res.status(500).json({ message: 'database error' });
     else res.status(200).json({ message: 'post updated' });
   });
 };
 
 exports.deletePost = (req, res) => {
   PostModel.findByIdAndRemove(req.params.id, (err, docs) => {
-    if (err || !docs) res.status(200).json({ message: 'database error' });
+    if (err || !docs) res.status(500).json({ message: 'database error' });
     else res.status(200).json({ message: 'post deleted' });
   });
 };
