be51e3081b4c13177e7a081bb906c5c62b5620e2
Unknown <abhirup.git@gmail.com>
2018-03-06 00:41:40 +0530

    'eslint' errors corrected and UI updated to center form in all devices.

--- app/auth/components/AuthPage/LoginForm.js
+++ app/auth/components/AuthPage/LoginForm.js
@@ -18,6 +18,7 @@ const LoginForm = ({
 }) => (
   <Grid centered>
     <Grid.Row>
+
       <Grid.Column width={11} className="login-container">
         <Grid>
           <Grid.Row>
@@ -25,25 +26,34 @@ const LoginForm = ({
             <Grid.Column width={9}>
               <div className="sidebar-info">
                 <Grid>
+
                   <Grid.Row>
                     <Grid.Column width={16}>
                       <Page height="256" />
                     </Grid.Column>
                   </Grid.Row>
+
                   <Grid.Row>
                     <Grid.Column width={16}>
                       <div>
+
                         <p className="sidebar-line-a">Welcome to</p>
                         <p className="sidebar-line-b">Internship Portal</p>
-                        <p className="sidebar-line-c">
-                          Don&#39;t have an account?
-                        </p>
+                        <p className="sidebar-line-c">Don&#39;t have an account?</p>
+
                         <div className="signup-button-wrapper">
-                          <Button as={Link} to="/signup" className="ecell-primary-button">Sign Up</Button>
+                          <Button
+                            as={Link}
+                            to="/signup"
+                            className="ecell-primary-button"
+                          >
+                            Sign Up
+                          </Button>
                         </div>
                       </div>
                     </Grid.Column>
                   </Grid.Row>
+
                 </Grid>
               </div>
             </Grid.Column>
@@ -51,89 +61,106 @@ const LoginForm = ({
             <Grid.Column width={7}>
               <div className="right-sidebar">
 
-              <div className="message-row">
-                {successMessage.length > 0 &&
-                  <Message success content={successMessage} />
-                }
-                {_.has(errors, 'summary') &&
-                  <Message error content={errors.summary} />
-                }
-              </div>
-              
+                {/*
+                  * For showing success and error message regarding sign-in
+                  * and sign-up.
+                  */}
+                <div className="message-row">
+                  {successMessage.length > 0 &&
+                    <Message success content={successMessage} />
+                  }
+                  {_.has(errors, 'summary') &&
+                    <Message error content={errors.summary} />
+                  }
+                </div>
 
-              <Grid>
-                <Grid.Row className="fingerprint-row">
-                  <Grid.Column width={6}></Grid.Column>
-                  <Grid.Column width={4}>
-                    <Fingerprint/>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-              
-              <Form onSubmit={onSubmit}>
-                <Form.Field className="signup-field">
-                  
-                  <label>E-mail</label>
-                  
-                  <input
-                    id="login-email"
-                    name="email"
-                    value={user.email}
-                    onChange={onChange}
-                    type="text"
-
-                    autoComplete="username"
-                  />
-                  
-                  <div className="pointer-message-error">
-                    {_.has(errors, 'email') &&
-                      <Label htmlFor="login-email" basic pointing className="ecell-red">
-                        {errors.email}
-                      </Label>
-                    }
-                  </div>
-                  
-                </Form.Field>
-
-                <Form.Field>
-                  <label>Password</label>
-                  
-                  <input
-                    id="login-password"
-                    name="password"
-                    value={user.password}
-                    onChange={onChange}
-                    type="password"
-
-                    autoComplete="current-password"
-                  />
-
-                  <div className="pointer-message-error">
-                    {_.has(errors, 'password') &&
-                      <Label htmlFor="login-password" basic pointing className="ecell-red">
-                        {errors.password}
-                      </Label>
-                    }
-                  </div>
-                  
-                </Form.Field>
-
+                {/* SVG fingerprint icon */}
                 <Grid>
-                  <Grid.Row className="button-row">
-                    <Grid.Column width={8}>
-                      <div className="forgot-password">
-                        Forgot Password
-                      </div>
-                    </Grid.Column>
-
-                    <Grid.Column width={8}>
-                        <div className="login-button-wrapper">
-                          <Button type="submit" className="ecell-primary-button">Log In</Button>
-                        </div>
+                  <Grid.Row className="fingerprint-row">
+                    <Grid.Column width={6} />
+                    <Grid.Column width={4}>
+                      <Fingerprint />
                     </Grid.Column>
                   </Grid.Row>
                 </Grid>
-              </Form>
+
+                {/* Log-In Form */}
+                <Form onSubmit={onSubmit}>
+
+                  <Form.Field className="signup-field">
+                    <label htmlFor="login-email">E-mail</label>
+                    <input
+                      id="login-email"
+                      name="email"
+                      value={user.email}
+                      onChange={onChange}
+                      type="text"
+                      autoComplete="username"
+                    />
+                    <div className="pointer-message-error">
+                      {_.has(errors, 'email') &&
+                        <Label
+                          htmlFor="login-email"
+                          basic
+                          pointing
+                          className="ecell-red"
+                        >
+                          {errors.email}
+                        </Label>
+                      }
+                    </div>
+                  </Form.Field>
+
+                  <Form.Field>
+                    <label htmlFor="login-password">Password</label>
+                    <input
+                      id="login-password"
+                      name="password"
+                      value={user.password}
+                      onChange={onChange}
+                      type="password"
+                      autoComplete="current-password"
+                    />
+                    <div className="pointer-message-error">
+                      {_.has(errors, 'password') &&
+                        <Label
+                          htmlFor="login-password"
+                          basic
+                          pointing
+                          className="ecell-red"
+                        >
+                          {errors.password}
+                        </Label>
+                      }
+                    </div>
+                  </Form.Field>
+
+                  <Grid>
+                    <Grid.Row className="button-row">
+
+                      {/* To be converted into a button/link in future when it works. */}
+                      <Grid.Column width={7} className="forgot-password-container">
+                        <div className="forgot-password">
+                          Forgot Password
+                        </div>
+                      </Grid.Column>
+
+                      <Grid.Column width={9} className="login-button-container">
+                        <div className="login-button-wrapper">
+                          <Button
+                            type="submit"
+                            className="ecell-primary-button"
+                          >
+                            Log In
+                          </Button>
+                        </div>
+                      </Grid.Column>
+
+                    </Grid.Row>
+                  </Grid>
+
+                </Form>
+
               </div>
             </Grid.Column>
 
