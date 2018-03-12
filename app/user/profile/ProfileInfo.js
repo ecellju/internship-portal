e26f6d04c03fa3c3598b7d4ad63c60db2287b1a9
Unknown <abhirup.git@gmail.com>
2018-03-12 22:01:52 +0530

    Style Update

--- app/user/profile/ProfileInfo.js
+++ app/user/profile/ProfileInfo.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Button, Card, Form, Label, Segment, List, Grid, Icon, Modal, Container, Header, TextArea, Text } from 'semantic-ui-react';
+import { Button, Form, Label, Segment, Grid, Modal, TextArea, Divider } from 'semantic-ui-react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import _ from 'lodash';
 
 import Auth from '../../auth/modules/Auth';
 import User from '../../auth/modules/User';
+import Add from '../../assets/add.svg';
 import Edit from '../../assets/edit.svg';
 import ModalSkillList from './ModalSkillList';
 import ProfileSkillList from './ProfileSkillList';
@@ -281,7 +282,7 @@ export default class ProfileInfo extends React.Component {
               trigger={
                 <Grid.Row textAlign="right">
                   <Grid.Column floated="right" className="general-edit-icon">
-                      <Edit height="16px" onClick={this.toggleEditability} />
+                    <Edit height="16px" onClick={this.toggleEditability} />
                   </Grid.Column>
                 </Grid.Row>
               }
@@ -496,11 +497,11 @@ export default class ProfileInfo extends React.Component {
             <Grid.Row columns={1} centered className="ecp-contact-row">
               <Grid.Column textAlign="center" className="ecp-contact-col">
                 <label htmlFor="user-email">
-                  {` E-mail: ${this.props.profile.Email} `}
+                  {` E-mail: ${this.props.profile.Email || ''} `}
                 </label>
                 <Label circular className="ecp-separator-label" htmlFor="seperator" color="black" />
                 <label htmlFor="user-phone">
-                  {` Phone: ${this.props.profile.contactNo} `}
+                  {` Phone: ${this.props.profile.contactNo || ''} `}
                 </label>
               </Grid.Column>
             </Grid.Row>
@@ -508,17 +509,17 @@ export default class ProfileInfo extends React.Component {
             <Grid.Row columns={1} centered className="ecp-dept-row">
               <Grid.Column textAlign="center" className="ecp-dept-col">
                 <label htmlFor="degree">
-                  {`${this.props.profile.degree} `}
+                  {`${this.props.profile.degree || ''} `}
                 </label>
                 <label htmlFor="branch">
-                  {`${this.props.profile.branch} `}
+                  {`${this.props.profile.branch || ''} `}
                 </label>
                 <label htmlFor="year">
-                  {` ${this.props.profile.joinYear} `}
+                  {` ${this.props.profile.joinYear || ''} `}
                 </label>
                 <Label circular className="ecp-separator-label" htmlFor="seperator" color="black" />
                 <label htmlFor="cgpa">
-                  {` CGPA: ${this.props.profile.cgpa}`}
+                  {` CGPA: ${this.props.profile.cgpa || ''}`}
                 </label>
               </Grid.Column>
             </Grid.Row>
@@ -585,15 +586,26 @@ export default class ProfileInfo extends React.Component {
             </Grid.Row>
           </Grid>
 
-          <br />
+          <Divider />
 
           {/* Internship Section */}
           <Grid className="general-info-section">
+            <Grid.Row centered className="ecp-section-row">
+              <Grid.Column className="ecp-section-col">
+                <label htmlFor="ecp-internship-header" className="ecp-section-header-label">
+                  Internships
+                </label>
+                <p className="ecp-section-text">
+                  {this.props.profile.internships || ''}
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+
             <Modal
               trigger={
-                <Grid.Row textAlign="right" className="section-modal-row">
-                  <Grid.Column floated="right" className="general-edit-icon">
-                    <Edit height="16px" onClick={this.toggleEditability} />
+                <Grid.Row centered className="ecp-section-row">
+                  <Grid.Column floated="right" className="general-edit-icon" onClick={this.toggleEditability} >
+                    <Add className="relative-24-d" height="16px" width="16px" />Edit Internship
                   </Grid.Column>
                 </Grid.Row>
               }
@@ -636,26 +648,28 @@ export default class ProfileInfo extends React.Component {
                 </Form.Field>
               </Modal.Content>
             </Modal>
-
-            <Grid.Row centered className="ecp-section-row">
-              <Grid.Column className="ecp-section-col">
-                <label htmlFor="ecp-internship-header" className="ecp-section-header-label">
-                  Internships
-                </label>
-                <p className="ecp-section-text">
-                  {this.props.profile.internships}
-                </p>
-              </Grid.Column>
-            </Grid.Row>
           </Grid>
+
+          <Divider />
 
           {/* Projects Section */}
           <Grid className="general-info-section">
+            <Grid.Row centered className="ecp-section-row">
+              <Grid.Column className="ecp-section-col">
+                <label htmlFor="ecp-projects-header" className="ecp-section-header-label">
+                  Projects &amp; Trainings
+                </label>
+                <p className="ecp-section-text">
+                  {this.props.profile.projects || ''}
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+
             <Modal
               trigger={
-                <Grid.Row textAlign="right" className="section-modal-row">
-                  <Grid.Column floated="right" className="general-edit-icon">
-                    <Edit height="16px" onClick={this.toggleEditability} />
+                <Grid.Row className="ecp-section-row">
+                  <Grid.Column className="general-edit-icon" onClick={this.toggleEditability} >
+                    <Add className="relative-24-d" height="16px" width="16px" />Edit Projects
                   </Grid.Column>
                 </Grid.Row>
               }
@@ -698,26 +712,53 @@ export default class ProfileInfo extends React.Component {
                 </Form.Field>
               </Modal.Content>
             </Modal>
-
-            <Grid.Row centered className="ecp-section-row">
-              <Grid.Column className="ecp-section-col">
-                <label htmlFor="ecp-projects-header" className="ecp-section-header-label">
-                  Projects &amp; Trainings
-                </label>
-                <p className="ecp-section-text">
-                  {this.props.profile.projects}
-                </p>
-              </Grid.Column>
-            </Grid.Row>
           </Grid>
+
+          <Divider />
 
           {/* Education Section */}
           <Grid className="general-info-section">
+            <Grid.Row centered width={16} className="ecp-section-row">
+              <Grid.Column className="ecp-section-col">
+                <label htmlFor="ecp-education-header" className="ecp-section-header-label">
+                  Education
+                </label>
+                <br />
+                <div className="ecp-subsection">
+                  <span htmlFor="ecp-subsection-header" className="ecp-subsection-header-label">
+                    Higher Secondary
+                  </span>
+                  <br />
+                  <span className="ecp-section-text">
+                    {`Year of Passing: ${this.props.profile.hsYear || ''}`}
+                  </span>
+                  <br />
+                  <span className="ecp-section-text">
+                    {`Marks: ${this.props.profile.hsMarks || ''}%`}
+                  </span>
+                </div>
+                <br />
+                <div className="ecp-subsection">
+                  <label htmlFor="ecp-subsection-header" className="ecp-subsection-header-label">
+                    Secondary
+                  </label>
+                  <br />
+                  <span className="ecp-section-text">
+                    {`Year of Passing: ${this.props.profile.secondaryYear || ''}`}
+                  </span>
+                  <br />
+                  <span className="ecp-section-text">
+                    {`Marks: ${this.props.profile.secondaryMarks || ''}%`}
+                  </span>
+                </div>
+              </Grid.Column>
+            </Grid.Row>
+
             <Modal
               trigger={
-                <Grid.Row textAlign="right" className="section-modal-row">
-                  <Grid.Column floated="right" className="general-edit-icon">
-                    <Edit height="16px" onClick={this.toggleEditability} />
+                <Grid.Row className="ecp-section-row">
+                  <Grid.Column className="general-edit-icon" onClick={this.toggleEditability} >
+                    <Add className="relative-24-d" height="16px" width="16px" />Edit Education
                   </Grid.Column>
                 </Grid.Row>
               }
@@ -798,51 +839,28 @@ export default class ProfileInfo extends React.Component {
                 </Form>
               </Modal.Content>
             </Modal>
-
-            <Grid.Row centered width={16} className="ecp-section-row">
-              <Grid.Column className="ecp-section-col">
-                <label htmlFor="ecp-education-header" className="ecp-section-header-label">
-                  Education
-                </label>
-                <br />
-                <div className="ecp-subsection">
-                  <span htmlFor="ecp-subsection-header" className="ecp-subsection-header-label">
-                    Higher Secondary
-                  </span>
-                  <br />
-                  <span className="ecp-section-text">
-                    {`Year of Passing: ${this.props.profile.hsYear}`}
-                  </span>
-                  <br />
-                  <span className="ecp-section-text">
-                    {`Marks: ${this.props.profile.hsMarks}%`}
-                  </span>
-                </div>
-                <br />
-                <div className="ecp-subsection">
-                  <label htmlFor="ecp-subsection-header" className="ecp-subsection-header-label">
-                    Secondary
-                  </label>
-                  <br />
-                  <span className="ecp-section-text">
-                    {`Year of Passing: ${this.props.profile.secondaryYear}`}
-                  </span>
-                  <br />
-                  <span className="ecp-section-text">
-                    {`Marks: ${this.props.profile.secondaryMarks}%`}
-                  </span>
-                </div>
-              </Grid.Column>
-            </Grid.Row>
           </Grid>
+
+          <Divider />
 
           {/* Positions of Responsibility Section */}
           <Grid className="general-info-section">
+            <Grid.Row width={16} centered className="ecp-section-row">
+              <Grid.Column className="ecp-section-col">
+                <label htmlFor="ecp-por-header" className="ecp-section-header-label">
+                  Positions of Responsibility
+                </label>
+                <p className="ecp-section-text">
+                  {this.props.profile.positionOfResponsibility || ''}
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+
             <Modal
               trigger={
-                <Grid.Row textAlign="right" className="section-modal-row">
-                  <Grid.Column floated="right" className="general-edit-icon">
-                    <Edit height="16px" onClick={this.toggleEditability} />
+                <Grid.Row className="ecp-section-row">
+                  <Grid.Column className="general-edit-icon" onClick={this.toggleEditability} >
+                    <Add className="relative-24-d" height="16px" width="16px" />Edit Positions
                   </Grid.Column>
                 </Grid.Row>
               }
@@ -885,26 +903,28 @@ export default class ProfileInfo extends React.Component {
                 </Form.Field>
               </Modal.Content>
             </Modal>
-
-            <Grid.Row width={16} centered className="ecp-section-row">
-              <Grid.Column className="ecp-section-col">
-                <label htmlFor="ecp-por-header" className="ecp-section-header-label">
-                  Positions of Responsibility
-                </label>
-                <p className="ecp-section-text">
-                  {this.props.profile.positionOfResponsibility}
-                </p>
-              </Grid.Column>
-            </Grid.Row>
           </Grid>
+
+          <Divider />
 
           {/* Work Samples */}
           <Grid className="general-info-section">
+            <Grid.Row width={16} centered className="ecp-section-row">
+              <Grid.Column className="ecp-section-col">
+                <label htmlFor="ecp-work-header" className="ecp-section-header-label">
+                  Work Samples
+                </label>
+                <p className="ecp-section-text">
+                  {this.props.profile.workSamples || ''}
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+
             <Modal
               trigger={
-                <Grid.Row textAlign="right" className="section-modal-row">
-                  <Grid.Column floated="right" className="general-edit-icon">
-                    <Edit height="16px" onClick={this.toggleEditability} />
+                <Grid.Row className="ecp-section-row">
+                  <Grid.Column className="general-edit-icon" onClick={this.toggleEditability} >
+                    <Add className="relative-24-d" height="16px" width="16px" />Edit Work Samples
                   </Grid.Column>
                 </Grid.Row>
               }
@@ -947,26 +967,30 @@ export default class ProfileInfo extends React.Component {
                 </Form.Field>
               </Modal.Content>
             </Modal>
-
-            <Grid.Row width={16} centered className="ecp-section-row">
-              <Grid.Column className="ecp-section-col">
-                <label htmlFor="ecp-work-header" className="ecp-section-header-label">
-                  Work Samples
-                </label>
-                <p className="ecp-section-text">
-                  {this.props.profile.workSamples}
-                </p>
-              </Grid.Column>
-            </Grid.Row>
           </Grid>
+
+          <Divider />
 
           {/* Co-Curricular Activities Section */}
           <Grid className="general-info-section">
+
+
+            <Grid.Row width={16} centered className="ecp-section-row">
+              <Grid.Column className="ecp-section-col">
+                <label htmlFor="ecp-co-curr-header" className="ecp-section-header-label">
+                  Co-Curricular Activities
+                </label>
+                <p className="ecp-section-text">
+                  {this.props.profile.coCurricularActivities || ''}
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+
             <Modal
               trigger={
-                <Grid.Row textAlign="right" className="section-modal-row">
-                  <Grid.Column floated="right" className="general-edit-icon">
-                    <Edit height="16px" onClick={this.toggleEditability} />
+                <Grid.Row className="ecp-section-row">
+                  <Grid.Column className="general-edit-icon" onClick={this.toggleEditability} >
+                    <Add className="relative-24-d" height="16px" width="16px" />Edit Co-curricular Activities
                   </Grid.Column>
                 </Grid.Row>
               }
@@ -1009,26 +1033,29 @@ export default class ProfileInfo extends React.Component {
                 </Form.Field>
               </Modal.Content>
             </Modal>
-
-            <Grid.Row width={16} centered className="ecp-section-row">
-              <Grid.Column className="ecp-section-col">
-                <label htmlFor="ecp-co-curr-header" className="ecp-section-header-label">
-                  Co-Curricular Activities
-                </label>
-                <p className="ecp-section-text">
-                  {this.props.profile.coCurricularActivities}
-                </p>
-              </Grid.Column>
-            </Grid.Row>
           </Grid>
+
+          <Divider />
 
           {/* Additional Details Section */}
           <Grid className="general-info-section">
+
+            <Grid.Row width={16} centered className="ecp-section-row">
+              <Grid.Column className="ecp-section-col">
+                <label htmlFor="ecp-additional-header" className="ecp-section-header-label">
+                  Additional Details
+                </label>
+                <p className="ecp-section-text">
+                  {this.props.profile.additionalDetails || ''}
+                </p>
+              </Grid.Column>
+            </Grid.Row>
+
             <Modal
               trigger={
-                <Grid.Row textAlign="right" className="section-modal-row">
-                  <Grid.Column floated="right" className="general-edit-icon">
-                    <Edit height="16px" onClick={this.toggleEditability} />
+                <Grid.Row className="ecp-section-row">
+                  <Grid.Column className="general-edit-icon" onClick={this.toggleEditability}>
+                    <Add className="relative-24-d" height="16px" width="16px" />Edit Additional Details
                   </Grid.Column>
                 </Grid.Row>
               }
@@ -1071,18 +1098,8 @@ export default class ProfileInfo extends React.Component {
                 </Form.Field>
               </Modal.Content>
             </Modal>
-
-            <Grid.Row width={16} centered className="ecp-section-row">
-              <Grid.Column className="ecp-section-col">
-                <label htmlFor="ecp-additional-header" className="ecp-section-header-label">
-                  Additional Details
-                </label>
-                <p className="ecp-section-text">
-                  {this.props.profile.additionalDetails}
-                </p>
-              </Grid.Column>
-            </Grid.Row>
           </Grid>
+
         </Grid.Column>
       </Grid>
     );
