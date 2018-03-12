a2d69a818995aafba4b9628caedf0b7b1e255052
Unknown <abhirup.git@gmail.com>
2018-03-12 12:43:08 +0530

    Profile Page UI Update

--- webpack.common.js
+++ webpack.common.js
@@ -31,15 +31,15 @@ module.exports = {
         test: /\.svg$/,
         use: [
           {
-            loader: "babel-loader"
+            loader: 'babel-loader',
           },
           {
-            loader: "react-svg-loader",
+            loader: 'react-svg-loader',
             options: {
-              jsx: true // true outputs JSX tags
-            }
-          }
-        ]
+              jsx: true, // true outputs JSX tags
+            },
+          },
+        ],
       },
     ],
   },
